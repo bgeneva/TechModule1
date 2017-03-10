2f3098a79dcffa18d856ee253011af122a253071
bgeneva <bgeneva@gmail.com>
2017-03-10 08:07:06 +0200

    Upload 04. Methods-Exercises

---
+++ 04. Methods-Exercises/HelloName/HelloName.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class HelloName
+    {
+        static void Main(string[] args)
+        {
+            string strName = Console.ReadLine();
+            Console.WriteLine(Hello(strName));
+
+        }
+
+        static string Hello(string name)
+        {
+            return "Hello, " + name + "!"; 
+        }
+    }
+
+  
+}
